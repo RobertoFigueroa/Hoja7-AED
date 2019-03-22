db0270fa5d990f8a00b2fbedb02c35e8f21fb93c
Michael Steven Chan Gonzalez <cha18562@uvg.edu.gt>
2019-03-22 11:50:34 -0600

    Reader

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,41 @@
 
 public class Main {
 
-    //little dictionary
+    public static void main(String[] args) {
+        String textTest;
 
+        ArrayList<String> text = new ArrayList<String>();
 
+        try {
+            FileReader file = new FileReader("test.txt");
+            BufferedReader reader = new BufferedReader(file);
+            while ((textTest = reader.readLine()) != null) {
+                text.add(textTest);
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Archivo no encontrado");
+        }
 
+        for (int i = 0; i < text.size(); i++) {
+            text.set(i, text.get(i).trim());
+        }
+
+        for (int i = 0; i < text.size(); i++) {
+            System.out.println("-" + text.get(i) + "-");
+        }
+        System.out.println(text.size());
+
+        textTest = "";
+        for (int i = 0; i < text.size(); i++) {
+            textTest += text.get(i) + " ";
+        }
+        textTest.trim();
+        textTest = textTest.replaceAll("  *", " ");
+        String[] tokens = textTest.split(" ");
+        System.out.println(textTest);
+        for (String s : tokens) {
+            System.out.println(s);
+        }
+    }
 }
