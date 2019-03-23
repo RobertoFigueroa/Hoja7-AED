055c69b26b3bc3f398327064306310754248bf37
mschang <cha18562@uvg.edu.gt>
2019-03-22 21:28:36 -0600

    Add dict and read txt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 /*
-*Universidad del Valle de Guatemala
+* Universidad del Valle de Guatemala
 * Hoja 7 Algoritmos y estructuras de datos
 * Roberto Figueroa 18306
-* Michael Chan 18
+* Michael Chan 18562
 *
 * */
 
@@ -10,8 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
         String textTest;
+        String dictionary;
 
         ArrayList<String> text = new ArrayList<String>();
+        ArrayList<String> dict = new ArrayList<String>();
 
         try {
             FileReader file = new FileReader("test.txt");
@@ -44,5 +46,47 @@ public class Main {
         for (String s : tokens) {
             System.out.println(s);
         }
-    }
+
+        try {
+            FileReader file = new FileReader("dict.txt");
+            BufferedReader reader = new BufferedReader(file);
+            while ((dictionary = reader.readLine()) != null) {
+                dict.add(dictionary);
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Archivo no encontrado");
+        }
+
+        for (int i = 0; i < dict.size(); i++) {
+            dict.set(i, dict.get(i).trim());
+        }
+
+        for (int i = 0; i < dict.size(); i++) {
+            System.out.println("-" + dict.get(i) + "-");
+        }
+        System.out.println(text.size());
+
+        dictionary = "";
+        for (int i = 0; i < dict.size(); i++) {
+            dictionary += dict.get(i) + " ";
+        }
+        dictionary.trim();
+        dictionary = dictionary.replaceAll("(","");
+        dictionary = dictionary.replaceAll(")","");
+        dictionary = dictionary.replaceAll("  *", " ");
+        String[] words = dictionary.split(" ");
+        System.out.println(dictionary);
+        String[] keys;
+        String[] values;
+        for (int i = 0,i < words.length(),i++) {
+            if((i % 2) == 0){
+                keys.add(words[i]);
+            } else {
+                values.add(words[i]);
+            }
+        }
+        for (int i = 0,i < keys.length(),i++) {
+            insertRec(keys[i],values[i]);
+        }
 }
